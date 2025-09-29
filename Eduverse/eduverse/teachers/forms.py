55d85e9c71a754565fd203e5169f61da2e73e8c2
IstiakNabil <128123853+IstiakNabil@users.noreply.github.com>
2025-09-29 22:01:51 +0600

    Delete Eduverse directory

--- Eduverse/eduverse/teachers/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import TeacherApplication
-
-class TeacherApplicationForm(forms.ModelForm):
-    class Meta:
-        model = TeacherApplication
-        fields = ["full_name", "email", "expertise", "years_experience", "bio", "linkedin_url", "resume"]
