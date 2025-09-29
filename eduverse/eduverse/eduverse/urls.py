6d87c24dc7549756f7e6de48ddb3fc21a0357e89
Rifah Tasfia <142161901+Rifah03@users.noreply.github.com>
2025-09-29 22:06:52 +0600

    Add files via upload

---
+++ eduverse/eduverse/eduverse/urls.py
@@ -0,0 +1,16 @@
+# eduverse/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),              # ✅ add this line
+    path("teachers/", include("teachers.urls")),  # your app
+    path("accounts/", include("django.contrib.auth.urls")),  # login/logout
+]
+
+# serve uploaded files (e.g., resumes) during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
