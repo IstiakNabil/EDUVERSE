0110a3743f25041e44724e0ed37de319296658a5
IstiakNabil <128123853+IstiakNabil@users.noreply.github.com>
2025-10-10 23:31:02 +0600

    bug fixed

--- Eduverse/eduverse/accounts/teachers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "teachers"
-
-urlpatterns = [
-    path("apply/", views.apply_as_teacher, name="apply"),
-    path("apply/success/", views.apply_success, name="apply_success"),
-]
